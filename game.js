d6aa3e80afaa557a674d74279325c86f4ddc2a87
Kurt Hammes <kilowatt11@cableone.net>
2016-06-23 23:52:49 -0600

    buttons hooked up. Player can choose character and attack. results register to screen

--- game.js
+++ game.js
@@ -31,7 +31,50 @@ function Fighter(name, health, hiPunch, lowPunch, hiKick, lowKick, special, defe
     fighters[name.toLowerCase()] = newFighter;
 }
 new Fighter('Blanca', 150, 10, 8, 20, 15, 50, .2);
-new Fighter('Ryu', 150, 8, 5, 12, 10, 30, .4);
-new Fighter('Ken', 150, 8, 5, 12, 10, 30, .4 );
+new Fighter('Ryu', 100, 8, 5, 12, 10, 30, .4);
+new Fighter('Ken', 100, 8, 5, 12, 10, 30, .4 );
 
-console.log(fighters);
+// console.log(fighters);
+
+function playerOneChoice(name){
+    players.player1 = fighters[name];
+    update(players.player1);
+    console.log(players.player1);
+};
+function playerTwoChoice(name){
+    players.player2 = fighters[name];
+    console.log(players.player2)
+    update(players.player2);
+
+}
+
+var attack1 = function(attackType,fighter,target){
+  target.health -=    fighter.attacks[attackType].damage-(fighter.attacks[attackType].damage * target.defense)
+  console.log(fighter.name + ' attacked ' + target.name + ' for ' + fighter.attacks.lowKick.damage + ' damage')
+    console.log(target.name + ' remaining health ' +target.health)
+    document.getElementById('playerTwoHealth').innerHTML = Math.round(target.health)
+    
+    
+}
+var attack2 = function(attackType,fighter,target){
+  target.health -=    fighter.attacks[attackType].damage-(fighter.attacks[attackType].damage * target.defense)
+  console.log(fighter.name + ' attacked ' + target.name + ' for ' + fighter.attacks.lowKick.damage + ' damage')
+    console.log(target.name + ' remaining health ' +target.health)
+    document.getElementById('playerOneHealth').innerHTML = Math.round(target.health);
+}
+
+function update (x){
+    if(x == players.player1){
+    var healthElem1 = document.getElementById('playerOneHealth').innerHTML = players.player1.health;
+    var nameElem1 = document.getElementById('playerOneName').innerHTML = players.player1.name;
+    }else{
+    var healthElem2 = document.getElementById('playerTwoHealth').innerHTML = players.player2.health;
+    var nameElem2 = document.getElementById('playerTwoName').innerHTML = players.player2.name;
+    }
+
+}
+
+
+
+
+
